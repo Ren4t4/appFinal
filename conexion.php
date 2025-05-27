7f92bf170a2768639b70f338447051c0515049ea
Ren4t4 <129637393+Ren4t4@users.noreply.github.com>
2025-05-27 12:27:00 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,13 @@
 <?php
 $host = "app-servidor21051484.mysql.database.azure.com";
-$usuario = "app-servidor21051484";
+$usuario = "app-servidor21051484@app-servidor21051484"; // incluye el dominio si Azure lo requiere
 $contrasena = "Pi-21051484";
 $base_datos = "calificaciones";
 
+// Crear conexión
 $conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
 
+// Verificar conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
